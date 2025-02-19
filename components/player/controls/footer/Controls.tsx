41c5b12da3b9d94ce3b0f4d1fd8cb14db36d8e05
MMinusOne <michael.business.lb@proton.me>
2025-02-19 21:16:01 +0200

    qualities expanded

--- components/player/controls/footer/Controls.tsx
+++ components/player/controls/footer/Controls.tsx
@@ -109,6 +109,7 @@ export default function Footer({ videoStore }: videoStoreOptions) {
   const toggleLanguages = () => {
     setCaptionsExpanded(false);
     setServersExpanded(false);
+    setQualitiesExpanded(false);
     setLanguagesExpanded(!languagesExpanded);
   };
 
